678479cc2ac50a6b6fa0c20d8167c07e5c3cd9ca
Pete Schaffner <peteschaffner@icloud.com>
2020-03-21 00:41:26 +0100

    Cleanup publish workflow

--- .github/actions/sync/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Start the SSH agent and load key
-eval "$(ssh-agent -s)"
-echo "$PRIVATE_KEY" | ssh-add
-
-# Publish
-site $GITHUB_WORKSPACE/Output --sync-to=pete@peteschaffner.com:www --exit-on-sync
