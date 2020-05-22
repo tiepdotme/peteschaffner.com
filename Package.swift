a43f42d01fddf0246961b127909c68a1bcb36fc2
Pete Schaffner <peteschaffner@icloud.com>
2020-05-22 12:38:36 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "PeteSchaffner", targets: ["PeteSchaffner"])
     ],
     dependencies: [
-        .package(url: "https://github.com/peteschaffner/Publish.git", .branch("master"))
+        .package(url: "https://github.com/JohnSundell/Publish", .branch("master"))
     ],
     targets: [
         .target(
