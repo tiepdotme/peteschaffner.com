461fa3818b100e3f17005864aa9b320cff3009d2
Pete Schaffner <peteschaffner@icloud.com>
2021-05-21 10:10:15 +0200

    Fix JavaScript error

--- Resources/js/welcome.js
+++ Resources/js/welcome.js
@@ -1,6 +1,8 @@
-let welcomeHeader = document.querySelector("#home h1");
-let day = new Date().toLocaleString(navigator.language, {  weekday: "long" });
+if (document.body.id == "home") {
+	let welcomeHeader = document.querySelector("#home h1");
+	let day = new Date().toLocaleString(navigator.language, {  weekday: "long" });
 
-if (welcomeHeader.textContent == "Welcome") {
-	welcomeHeader.textContent = "Happy " + day;
+	if (welcomeHeader.textContent == "Welcome") {
+		welcomeHeader.textContent = "Happy " + day;
+	}
 }
