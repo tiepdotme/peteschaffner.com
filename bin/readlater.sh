afa5a971d20ed0a0aeb349be700661cea8b206d5
Pete Schaffner <peteschaffner@icloud.com>
2021-08-18 05:54:51 +0200

    Update read later script to use new title format

--- bin/readlater.sh
+++ bin/readlater.sh
@@ -8,7 +8,7 @@ title=$(curl -L $url -so - | ruby -rnokogiri -e 'puts Nokogiri::HTML(readlines.j
 
 if [ -n "$title" ]; then
 	git pull --rebase
-	echo -e "---\ndate: $post_date $(date +%H:%M)\nlink: $url\n---\n\n# $title" >> $basedir/../Content/readlater/$post_date-$(date +%H%M).md
+	echo -e "---\ntitle: $title\ndate: $post_date $(date +%H:%M)\nlink: $url\n---\n" >> $basedir/../Content/readlater/$post_date-$(date +%H%M).md
 	git add Content/readlater/
 	git commit -m"Add read later item"
 	git push
